326e81b20f81945eefa3372aa6b9718cb17f530d
Ayo100x <ayomideadelooye@gmail.com>
2026-09-18 03:12:07 +0100

    feat: add MarketPlacePage route to App component

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
 import ProductPage from "./pages/product/productPage";
 import LookPage from "./pages/look/LookPage";
+import MarketPlacePage from "./pages/marketPlace/MarketPlacePage";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/product/:id" element={<ProductPage />} />
       <Route path="/look/:id" element={<LookPage />} />
+      <Route path="/marketplace" element={<MarketPlacePage />} />
     </Routes>
   );
 }
